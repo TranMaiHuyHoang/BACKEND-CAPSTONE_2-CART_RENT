f114ed0ebe3b3d17e3c286a272eb0f10dc43b5da
Tran Mai Huy Hoang <tranmhuyhoang@dtu.edu.vn>
2026-02-25 11:33:12 +0700

    model backend (gồm : contact,interaction, payment,userlocation,vehicle,vehicleHire,vehicleLocation

---
+++ src/models/userLocation.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userLocationSchema = new Schema({
+    address: String,
+    latitude: String,
+    longitude: String,
+    plus_code: String,
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    deleted_at: String
+},
+    {
+        timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+    }
+);
+
+module.exports = mongoose.model('UserLocation', userLocationSchema);
---
+++ src/models/vehicleLocation.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const vehicleLocationSchema = new Schema({
+    address: String,
+    latitude: String,
+    longitude: String,
+    plus_code: String,
+    vehicle: { type: Schema.Types.ObjectId, ref: 'Vehicle', required: true },
+},
+    {
+        timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+    }
+);
+
+module.exports = mongoose.model('VehicleLocation', vehicleLocationSchema);
