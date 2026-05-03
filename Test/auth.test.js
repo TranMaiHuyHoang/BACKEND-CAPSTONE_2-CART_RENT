db216c83cb8a5fa5553a6d46b97a17f25f68790b
Tran Mai Huy Hoang <tranmhuyhoang@dtu.edu.vn>
2026-05-03 14:46:02 +0700

    add test : auth

---
+++ Test/auth.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest');
+const app = require('../app'); // file Express app của bạn
+
+describe('Auth API Tests', () => {
+    let token;
+
+    const testUser = {
+        email: 'testuser@example.com',
+        password: '12345678',
+        name: 'Test User',
+    };
+
+    
+});
