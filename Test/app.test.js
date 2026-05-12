a42fb87ded638522d482b3b5c94dad13a3215fe0
dangtuanminh <tuanminhtg004@gmail.com>
2026-05-12 12:34:42 +0700

    GET login API return token in app js

--- Test/app.test.js
+++ Test/app.test.js
@@ -7,4 +7,10 @@ describe("JWT API", () => {
     process.env.JWT_EXPIRES_IN = "1h";
   });
 
+  test("GET /login should return a token", async () => {
+    const res = await request(app).get("/login");
+    expect(res.statusCode).toBe(200);
+    expect(res.body.token).toBeDefined();
+  });
+
 });
